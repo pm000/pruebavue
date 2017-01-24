56b5f7ae9b27691c464df794ab74d5de0c6a639f
zeratulmdq <colomdq85@hotmail.com>
2017-01-24 19:21:51 -0300

    Primera version

---
+++ src/cabecera.vue
@@ -0,0 +1,40 @@
+<template>
+	<div class="cabecera">
+		<span @click="hola" class="titulo">{{ msg }}</span>
+		<span @click="chau" class="titulo">Chau</span>
+	</div>
+</template>
+<script>
+	import Moment from 'moment'
+
+	export default {
+		name: 'cabecera',
+		data() {
+			return {
+				msg: Moment().add(7, 'days')
+			}
+		},
+		methods: {
+			hola() {
+				console.log("HOLA");
+			},
+			chau() {
+				console.log("CHAU");
+			}
+		}
+	}
+</script>
+<style>
+	.cabecera {
+		position: fixed;
+		top: 0;
+		left: 0;
+		background-color: #F00;
+		width: 100%;
+	}
+
+	.titulo {
+		display: inline-block;
+		margin: 10px;
+	}
+</style>
